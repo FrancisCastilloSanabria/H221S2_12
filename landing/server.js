66043e7f4d84adb65589a77fab8a8f6b4542f8f6
Francis Castillo Sanabria <francis.castillo@vallegrande.edu.pe>
2022-12-14 21:23:37 +0000

    a

---
+++ landing/server.js
@@ -0,0 +1,14 @@
+
+const express = require('express');
+const app = express();
+
+//recursos
+app.use(express.static(__dirname+'/'));
+
+//Enrutamiento
+app.get('/',(req,res) => {
+    res.sendFile("/home/ubuntu/projects/landing/menu.html")
+});
+
+app.listen(3000);
+console.log('server on port 3000');
