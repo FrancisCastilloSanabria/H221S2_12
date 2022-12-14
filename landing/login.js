6e4fcb5848ab3098b9a268d7c88d6fd94f3b9546
Francis Castillo <francis.castillo@vallegrande.edu.pe>
2022-12-14 17:02:03 -0500

    s

--- landing/login.js
+++ landing/login.js
@@ -6,7 +6,7 @@ const path = require('path');
 const connection = mysql.createConnection({
 	host     : 'localhost',
 	user     : 'root',
-	password : '',
+	password : 'francis144',
 	database : 'loginsocket'
 });
 
